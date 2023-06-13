58a718e78343f9cafb4d34a6729d6f939a05a2c2
Timothy7310 <penlist127@gmail.com>
2023-06-13 18:20:03 +0300

    feat: add fs/create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,45 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  // 1
+  fs.access(path.join(__dirname, "/files", "fresh.txt"), (err) => {
+    if (err) {
+      fs.writeFile(
+        path.join(__dirname, "/files", "fresh.txt"),
+        "I am fresh and young",
+        (err) => {
+          if (err) {
+            throw Error(err.message);
+          }
+        }
+      );
+    } else {
+      throw Error("FS operation failed");
+    }
+  });
+
+  // 2
+  // fs.stat(path.join(__dirname, "/files", "fresh.txt"), (err) => {
+  //   if (err === null) {
+  //     throw Error("FS operation failed");
+  //   } else if (err.code === "ENOENT") {
+  //     fs.writeFile(
+  //       path.join(__dirname, "/files", "fresh.txt"),
+  //       "I am fresh and young",
+  //       (err) => {
+  //         if (err) {
+  //           throw Error(err.message);
+  //         }
+  //       }
+  //     );
+  //   } else {
+  //     console.log("WTF? ", err.code);
+  //   }
+  // });
 };
 
 await create();
