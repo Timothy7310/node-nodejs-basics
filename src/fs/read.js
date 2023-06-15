7aa7efd1ae2e93d85eb9511dc1a136a7bc518e26
Timothy7310 <penlist127@gmail.com>
2023-06-15 17:30:47 +0300

    feat: add fs/read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,30 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pathToFile = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  // 1
+  //   fs.readFile(pathToFile, "utf-8", (err, data) => {
+  //     if (err && err.code === "ENOENT") {
+  //       throw Error("FS operation failed");
+  //     } else {
+  //       console.log(data);
+  //     }
+  //   });
+
+  //   2
+  fs.access(pathToFile, (err) => {
+    if (err) {
+      throw Error("FS operation failed");
+    } else {
+      const stream = fs.createReadStream(pathToFile, "utf-8");
+      stream.on("data", (data) => console.log(data));
+    }
+  });
 };
 
 await read();
