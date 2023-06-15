c6a81fc1f0dbb14bb5b667b329d6d079ca45bf97
Timothy7310 <penlist127@gmail.com>
2023-06-15 17:14:07 +0300

    feat: add fs/delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pathToFile = path.join(__dirname, path.sep, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  // 1
+  fs.unlink(pathToFile, (err) => {
+    if (err && err.code === "ENOENT") {
+      throw Error("FS operation failed");
+    }
+  });
+
+  // 2
+  //   fs.rm(pathToFile, (err) => {
+  //     if (err && err.code === "ENOENT") {
+  //       throw Error("FS operation failed");
+  //     }
+  //   });
 };
 
 await remove();
