aa3d3b19d8f4671e47a2207644af21386aa3bce4
Timothy7310 <penlist127@gmail.com>
2023-06-15 18:26:40 +0300

    feat: add cli/args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = process.argv
+    .slice(2)
+    .map((x, index, arr) => {
+      if ((index + 1) % 2 === 0) {
+        return [arr[index - 1], x];
+      }
+    })
+    .filter((x) => x)
+    .map((x) => x.join(" is "))
+    .join(", ");
+  console.log(result);
 };
 
 parseArgs();
