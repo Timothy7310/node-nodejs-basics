31024c0bedf1893fe1c104943d87082213507be1
Timothy7310 <penlist127@gmail.com>
2023-06-16 15:11:44 +0300

    feat: add streams/read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pathToFile = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(pathToFile);
+  stream.on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
 
 await read();
