deeb27b6c7f51dfcfb23481958e7f843383d5f9e
Timothy7310 <penlist127@gmail.com>
2023-06-16 16:05:50 +0300

    feat: add stream/transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(data, _encoding, cb) {
+      cb(null, `${`${data}`.split("").reverse().join("")}\n\n`);
+    },
+  });
+
+  process.stdin.pipe(transformStream).on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
 
 await transform();
