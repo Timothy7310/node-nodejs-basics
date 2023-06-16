7143a7d2fde7a0ab651c491833e6abce400a65c7
Timothy7310 <penlist127@gmail.com>
2023-06-16 15:26:33 +0300

    feat: add streams/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
-const write = async () => {
-    // Write your code here 
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pathToFile = path.join(__dirname, "files", "fileToWrite.txt");
+
+const transform = async () => {
+  const stream = fs.createWriteStream(pathToFile);
+  process.stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
 
-await write();
+await transform();
