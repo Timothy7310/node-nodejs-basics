e77b02aba64af17b5da511b531b07b3f26760b7e
Timothy7310 <penlist127@gmail.com>
2023-06-16 14:54:44 +0300

    feat: add hash/calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import crypto from "crypto";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(pathToFile, "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(crypto.createHash("sha256").update(data).digest("hex"));
+  });
 };
 
 await calculateHash();
