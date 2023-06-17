cb551b7672b661d2eaaf93fcc28eaa7be896b572
timothy <penlist127@gmail.com>
2023-06-17 18:48:09 +0300

    feat: add worker_threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+    const baseNum = 10;
+    const countCore = os.cpus().length;
+    const promiseArr = [...Array(countCore).keys()]
+    .map((item) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(pathToFile, {
+                workerData: baseNum + item,
+            })
+            worker.on('message', (data) => {
+                resolve({status: 'resolved', data})
+            })
+            worker.on('error', (_err) => {
+                reject({status: 'error', data: null})
+            })
+        })
+    })
+
+    Promise.all(promiseArr).then((res) => console.log(res)).catch((res) => console.log(res))
 };
 
 await performCalculations();
